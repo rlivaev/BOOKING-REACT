65e70ef05a9707814829ef8996c2a6c0e7ff0daa
Rustam <rustam@MacBook-Pro-Rustam.local>
2023-02-08 16:23:24 +0600

    add components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 
 import { BrowserRouter,Routes,Route, Router } from "react-router-dom";
 import Home from './pages/home/Home.jsx';
+import Hotel from "./pages/hotel/Hotel.jsx";
 import List from "./pages/list/List.jsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element = { <Home/> }/>
           <Route path="/hotels" element = {<List/>}/>
+          <Route path="/hotel/:id" element = {<Hotel/>}/>
       </Routes>
     </BrowserRouter>
   );
